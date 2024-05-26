b658685907dd17b91687468748b4ce2fa6797d14
Samms206 <sh2069936@gmail.com>
2024-05-26 16:30:25 +0700

    updated

--- resources/views/layout/rollback/customer.blade.php
+++ resources/views/layout/rollback/customer.blade.php
@@ -3,10 +3,10 @@
     <!-- Container Fluid-->
     <div class="container-fluid" id="container-wrapper">
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Customer</h1>
+            <h1 class="h3 mb-0 text-gray-800">Return Customer</h1>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="./">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Customer</li>
+                <li class="breadcrumb-item active" aria-current="page">Return Customer</li>
             </ol>
         </div>
 
@@ -15,117 +15,6 @@
             <!-- DataTable with Hover -->
             <div class="col-lg-12">
                 <div class="card mb-4">
-                    <!-- Modal Add-->
-                    <div class="modal fade" id="addModal" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Tambah Customer</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <form method="POST" action="{{ route('add-customer') }}" enctype="multipart/form-data">
-                                        @csrf
-                                        <div class="form-group">
-                                            <label for="name">Nama</label>
-                                            <input type="text"  required class="form-control" id="name" name="name"
-                                                placeholder="Nama">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="address">Telepon</label>
-                                            <input type="text"  required class="form-control" id="phone" name="phone"
-                                                placeholder="Telepon">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="address">Alamat</label>
-                                            <input type="text"  required class="form-control" id="address" name="address"
-                                                placeholder="Alamat">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Simpan</button>
-                                    </form>
-
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-outline-primary"
-                                        data-dismiss="modal">Tutup</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- end Modal --}}
-                    <!-- Modal Edit-->
-                    <div class="modal fade" id="editModalUser" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Tambah User</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <form method="POST" action="" enctype="multipart/form-data">
-                                        @csrf
-                                        @method('PUT')
-                                        <div class="form-group">
-                                            <label for="name">Nama</label>
-                                            <input type="text"  required class="form-control" id="editUserName" name="name"
-                                                placeholder="Nama">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="address">Telepon</label>
-                                            <input type="text"  required class="form-control" id="editUserPhone" name="phone"
-                                                placeholder="Telepon">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="address">Alamat</label>
-                                            <input type="text"  required class="form-control" id="editUserAddress" name="address"
-                                                placeholder="Alamat">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Simpan</button>
-                                    </form>
-
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-outline-primary"
-                                        data-dismiss="modal">Tutup</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- end Edit --}}
-                    <!-- Modal Delete-->
-                    <div class="modal fade" id="deleteModalUser" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Anda yakin ingin menghapus data ini?</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    Apakah Anda yakin ingin menghapus data ini?
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                                    <!-- Tombol Konfirmasi Hapus -->
-                                    <form id="deleteFormUser" method="POST"
-                                        action="{{ route('delete-customer', ':user_id') }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Hapus</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- end Modal --}}
                     <div class="table-responsive p-3">
                         <table class="table align-items-center table-flush table-hover" id="dataTableHover">
                             <thead class="thead-light">
--- resources/views/layout/rollback/product.blade.php
+++ resources/views/layout/rollback/product.blade.php
@@ -3,10 +3,10 @@
     <!-- Container Fluid-->
     <div class="container-fluid" id="container-wrapper">
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Barang</h1>
+            <h1 class="h3 mb-0 text-gray-800">Return Barang</h1>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="./">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Barang</li>
+                <li class="breadcrumb-item active" aria-current="page">Return Barang</li>
             </ol>
         </div>
 
@@ -15,128 +15,6 @@
             <!-- DataTable with Hover -->
             <div class="col-lg-12">
                 <div class="card mb-4">
-                    <!-- Modal Add-->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Tambah Barang</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <form method="POST" action="{{ route('add-product') }}" enctype="multipart/form-data">
-                                        @csrf
-                                        <div class="form-group">
-                                            <label for="namaProduk">Nama Produk</label>
-                                            <input type="text" required class="form-control" id="namaProduk" name="name"
-                                                placeholder="Nama Produk">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="touchSpin3">Stok</label>
-                                            <div class="input-group touch-spin">
-                                                <span class="input-group-btn input-group-prepend">
-                                                </span>
-                                                <input style="text-align: center" id="touchSpin3" type="text" required
-                                                    value="0" name="stock" class="form-control">
-                                                <span class="input-group-btn input-group-append">
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="hrgjual">Harga Jual</label>
-                                            <input type="text" required class="form-control" id="hrgjual" placeholder="Rp.-"
-                                                name="hrg_jual">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="hrgeli">Harga Beli</label>
-                                            <input type="text" required class="form-control" id="hrgeli" placeholder="Rp.-"
-                                                name="hrg_beli">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Simpan</button>
-                                    </form>
-
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-outline-primary"
-                                        data-dismiss="modal">Tutup</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- end Modal --}}
-                    <!-- Modal Delete-->
-                    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Anda yakin ingin menghapus data ini?</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    Apakah Anda yakin ingin menghapus data ini?
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                                    <!-- Tombol Konfirmasi Hapus -->
-                                    <form id="deleteForm" method="POST"
-                                        action="{{ route('delete-product', ':barang_id') }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Hapus</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- end Modal --}}
-                    <!-- Modal Edit -->
-                    <div class="modal fade" id="editModal" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Edit Barang</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <form method="POST" action="">
-                                        @csrf
-                                        @method('PUT')
-                                        <div class="form-group">
-                                            <label for="editNamaProduk">Nama Produk</label>
-                                            <input type="text" required class="form-control" id="editNamaProduk"
-                                                name="edit_name" placeholder="Nama Produk">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="editStok">Stok</label>
-                                            <input type="text" required class="form-control" id="editStok" name="edit_stock"
-                                                placeholder="Stok">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="editHargaJual">Harga Jual</label>
-                                            <input type="text" required class="form-control" id="editHargaJual"
-                                                name="edit_hrg_jual" placeholder="Harga Jual">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="editHargaBeli">Harga Beli</label>
-                                            <input type="text" required class="form-control" id="editHargaBeli"
-                                                name="edit_hrg_beli" placeholder="Harga Beli">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Simpan</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- end modal edit --}}
                     <div class="table-responsive p-3">
                         <table class="table align-items-center table-flush table-hover" id="dataTableHover">
                             <thead class="thead-light">
