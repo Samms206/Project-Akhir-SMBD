fec387995597030b5575a44dc0ba69a5532c8739
Samms206 <sh2069936@gmail.com>
2024-05-28 22:31:24 +0700

    updated

--- resources/views/layout/rollback/product.blade.php
+++ resources/views/layout/rollback/product.blade.php
@@ -3,10 +3,10 @@
     <!-- Container Fluid-->
     <div class="container-fluid" id="container-wrapper">
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Return Barang</h1>
+            <h1 class="h3 mb-0 text-gray-800">Rollback Barang</h1>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="./">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Return Barang</li>
+                <li class="breadcrumb-item active" aria-current="page">Rollback Barang</li>
             </ol>
         </div>
 
@@ -36,13 +36,22 @@
                                         <td>{{ $barang->hrg_beli }}</td>
                                         <td>{{ $barang->created_at }}</td>
                                         <td>
-                                            <form action="{{ route('rollback-product-execute', $barang->id) }}" method="POST" >
-                                                @csrf
-                                                @method('PUT')
-                                                <button type="submit" class="btn btn-warning edit-btn">
-                                                    <i class="fas fa-recycle"></i>
-                                                </button>
-                                            </form>
+                                            <div class="row" style="gap: 6px">
+                                                <form action="{{ route('rollback-product-execute', $barang->id) }}" method="POST" >
+                                                    @csrf
+                                                    @method('PUT')
+                                                    <button type="submit" class="btn btn-warning edit-btn">
+                                                        <i class="fas fa-recycle"></i>
+                                                    </button>
+                                                </form>
+                                                <form action="{{ route('delete-product', $barang->id) }}" method="POST" >
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger edit-btn">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </form>
+                                            </div>
                                         </td>
                                     </tr>
                                 @endforeach
